eb61d7d668de45ac65cb4748db568943442be80f
Ubuntu <ubuntu@ip-172-31-51-232.ec2.internal>
2021-11-05 16:09:42 +0000

    added file

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,5 +1,8 @@
+// Copyright (c) 2019 St. Mother Teresa HS All rights reserved.
+
 #include <iostream>
 
-int main () {
-    std::cout << "Hello, World!";
+int main()
+{
+	std::cout << "Hello, World!";
 }
